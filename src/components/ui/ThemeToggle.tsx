a9b0c6c4467ddca2a2b773ada7e5d4f3d1b424c5
Theo Maerten <theo.maerten@sga-industires.com>
2025-06-24 15:23:11 +0200

    réparation du bouton toggle theme

--- src/components/ui/ThemeToggle.tsx
+++ src/components/ui/ThemeToggle.tsx
@@ -49,7 +49,7 @@ export function ThemeToggle() {
               ? faMoon
               : faSun
           }
-          className="w-6 h-6 text-[var(--white)] transition-all duration-300 transform group-xl:hover:rotate-180 group-xl:hover:text-[var(--background)]"
+          className="w-6 h-6 text-[var(--white)] transition-all duration-300 transform group-hover:rotate-180 group-hover:text-[var(--background)]"
         />
       </button>
     </div>
