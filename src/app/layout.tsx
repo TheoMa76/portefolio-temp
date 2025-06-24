7bb8a1009f9c7e17cf76dc8b08965d71d2762ad1
Theo Maerten <theo.maerten@sga-industires.com>
2025-06-24 13:29:13 +0200

    fixed image favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,7 +53,7 @@ export default function RootLayout({
         <head>
           <meta charSet="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <link rel="icon" type="image/x-icon" href="/images/favicon.ico" />
+          <link rel="icon" type="image/x-icon" href="/favicon.ico" />
           <link rel="canonical" href={`${process.env.SITE_URL}`} />
           <title>{String(metadata.title ?? "")}</title>
           <meta name="description" content={metadata.description ?? ""} />
