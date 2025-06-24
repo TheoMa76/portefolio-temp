4d13ba0202fd4391a7c6020877e6b855a0d2ee6a
Theo Maerten <theo.maerten@sga-industires.com>
2025-06-24 16:49:58 +0200

    Fix custom footer responsvie

--- src/components/layout/CustomFooter.tsx
+++ src/components/layout/CustomFooter.tsx
@@ -5,45 +5,47 @@ import { faCircleUser} from "@fortawesome/free-solid-svg-icons";
 
 export default function CustomFooter() {
   return (
-    <div className="w-full mt-30 bg-[var(--primary)] text-[var(--white)] py-6 min-h-[150px] shadow-lg border-t-2 border-[var(--secondary)] overflow-hidden glowblue">
-      <div className="container mx-auto grid gap-6 md:grid-cols-3 items-center text-center md:text-left">
-        <div className="space-y-1 text-[var(--background)]">
-          <p className="font-semibold">Contact</p>
-          <p>theoma.dev@gmail.com</p>
-        </div>
+    <div className="w-full mt-30 bg-[var(--primary)] text-[var(--white)] py-8 min-h-[150px] shadow-lg border-t-2 border-[var(--secondary)] overflow-hidden glowblue relative">
+      <div className="container mx-auto px-4">
+        <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between gap-6">
+          <div className="text-center lg:text-left space-y-1 text-[var(--background)]">
+            <p className="font-semibold">Contact</p>
+            <p>theoma.dev@gmail.com</p>
+          </div>
 
-        <div className="flex justify-center space-x-6">
-          <Link
-            href="https://github.com/TheoMa76"
-            target="_blank"
-            rel="noopener noreferrer"
-            ariaLabel="Lien vers le GitHub de Théo Maerten"
-            icone={<FontAwesomeIcon icon={faGithub} />}
-          >
-            GitHub
-          </Link>
-          <Link
-            href="https://www.linkedin.com/in/th%C3%A9o-maerten-a8161725a/"
-            target="_blank"
-            rel="noopener noreferrer"
-            ariaLabel="Lien vers le LinkedIn de Théo Maerten"
-            icone={<FontAwesomeIcon icon={faLinkedin} />}
-          >
-            LinkedIn
-          </Link>
-          <Link
-            href="https://www.malt.fr/profile/theomaerten"
-            target="_blank"
-            rel="noopener noreferrer"
-            ariaLabel="Lien vers le compte Malt de Théo Maerten"
-            icone={<FontAwesomeIcon icon={faCircleUser} />}
-          >
-            Malt
-          </Link>
-        </div>
+          <div className="flex flex-col items-center space-y-4 lg:flex-row lg:absolute lg:left-1/2 lg:transform lg:-translate-x-1/2 lg:space-y-0 lg:space-x-6">
+            <Link
+              href="https://github.com/TheoMa76"
+              target="_blank"
+              rel="noopener noreferrer"
+              ariaLabel="Lien vers le GitHub de Théo Maerten"
+              icone={<FontAwesomeIcon icon={faGithub} />}
+            >
+              GitHub
+            </Link>
+            <Link
+              href="https://www.linkedin.com/in/th%C3%A9o-maerten-a8161725a/"
+              target="_blank"
+              rel="noopener noreferrer"
+              ariaLabel="Lien vers le LinkedIn de Théo Maerten"
+              icone={<FontAwesomeIcon icon={faLinkedin} />}
+            >
+              LinkedIn
+            </Link>
+            <Link
+              href="https://www.malt.fr/profile/theomaerten"
+              target="_blank"
+              rel="noopener noreferrer"
+              ariaLabel="Lien vers le compte Malt de Théo Maerten"
+              icone={<FontAwesomeIcon icon={faCircleUser} />}
+            >
+              Malt
+            </Link>
+          </div>
 
-        <div className="text-md md:text-right text-[var(--background)]">
-          © {new Date().getFullYear()} Théo Maerten. Tous droits réservés.
+          <div className="text-center lg:text-right text-[var(--background)]">
+            © {new Date().getFullYear()} Théo Maerten. Tous droits réservés.
+          </div>
         </div>
       </div>
     </div>
--- src/components/ui/Link.tsx
+++ src/components/ui/Link.tsx
@@ -11,13 +11,12 @@ type LinkProps = {
 
 const Link = (props: LinkProps) => {
   return (
-    
     <a
       href={props.href}
       aria-label={props.ariaLabel}
       target={props.target}
       rel={props.rel}
-      className='text-[var(--white)] xl:hover:text-[var(--background)] transition-colors duration-300 flex items-center gap-2 px-4 py-2 rounded-2xl bg-[var(--background)] xl:hover:bg-[var(--secondary)] shadow-lg xl:hover:shadow-xl'
+      className='text-[var(--white)] xl:hover:text-[var(--background)] transition-colors duration-300 flex items-center justify-center gap-2 px-4 py-2 rounded-2xl bg-[var(--background)] xl:hover:bg-[var(--secondary)] shadow-lg xl:hover:shadow-xl min-w-[120px] text-center'
     >
       {props.icone && <span className="icon">{props.icone}</span>}
       {props.children}
